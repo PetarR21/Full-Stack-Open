a62078e755b6c7e0946e5e3d564313e6bd5f016c
PetarR21 <petar.r.rakocevic@gmail.com>
2023-02-03 10:24:07 +0100

    Finished exercise 2.10

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Note from './components/Note';
 
-function App() {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('');
+  const [showAll, setShowAll] = useState(true);
+
+  const addNote = (event) => {
+    event.preventDefault();
+
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote('');
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button
+          onClick={() => {
+            setShowAll(!showAll);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={({ target }) => {
+            setNewNote(target.value);
+          }}
+        />
+        <button type='submit'>save</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import Filter from './components/Filter';
+
+import PersonForm from './components/PersonForm';
+import Persons from './components/Persons';
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    if (persons.map((person) => person.name).includes(newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+
+    const personObject = {
+      name: newName,
+      number: newNumber,
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName('');
+    setNewNumber('');
+  };
+
+  const personsToShow = persons.filter(
+    (person) =>
+      person.name.trim().toLowerCase().includes(filter.trim().toLowerCase()) ||
+      person.number.trim().toLowerCase().includes(filter.trim().toLowerCase())
+  );
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter filter={filter} setFilter={setFilter} />
+
+      <h3>Add a new</h3>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+      />
+
+      <h3>Numbers</h3>
+      <Persons persons={personsToShow} />
+    </div>
+  );
+};
+
+export default App;
