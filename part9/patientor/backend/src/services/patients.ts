6e8570b246b81db481e42d35fa18bec7915115ab
PetarR21 <petar.r.rakocevic@gmail.com>
2024-02-01 13:07:32 +0100

    Finished exercise 9.11

---
+++ part9/patientor/backend/src/services/patients.ts
@@ -0,0 +1,14 @@
+import { PublicPatient } from '../types';
+import patients from '../../data/patients';
+
+const getPublicPatients = (): PublicPatient[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+export default { getPublicPatients };
